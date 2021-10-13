b0ff806a7794c6755e1c52f6491c6e23a67e1c0e
kea-27 <rladmsdk227@gmail.com>
2021-10-14 03:21:36 +0900

    updateBoard, deleteBoard 구현

--- src/main/java/board/board/mapper/BoardMapper.java
+++ src/main/java/board/board/mapper/BoardMapper.java
@@ -17,4 +17,8 @@ public interface BoardMapper {
 
 	BoardDto selectBoardDetail(int boardIdx) throws Exception;
 
+	void updateBoard(BoardDto board) throws Exception;
+
+	void deleteBoard(int boardIdx) throws Exception;
+
 }
