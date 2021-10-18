657ddbb27dc47da86d2f3f7583dc6433c394178f
kea-27 <rladmsdk227@gmail.com>
2021-10-19 00:56:40 +0900

    인코딩 설정 수정

--- src/main/java/board/configuration/WebMvcConfiguration.java
+++ src/main/java/board/configuration/WebMvcConfiguration.java
@@ -1,17 +1,10 @@
 package board.configuration;
 
-import java.nio.charset.Charset;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import board.interceptor.LoggerInterceptor;
-import ch.qos.logback.core.filter.Filter;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer{
@@ -21,19 +14,18 @@ public class WebMvcConfiguration implements WebMvcConfigurer{
 		registry.addInterceptor(new LoggerInterceptor());
 	}
 	
-	@SuppressWarnings("rawtypes")
-	@Bean
-	public Filter characterEncodingFilter() {
-		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-		
-		characterEncodingFilter.setEncoding("UTF-8");
-		characterEncodingFilter.setForceEncoding(true);
-		
-		return characterEncodingFilter();
-	}
-	
-	@Bean
-	public HttpMessageConverter<String> responseBodyConverter() {
-		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
-	}
+	/* 스프링부트 2.1.x 버전에는 이미 인코딩 필터가 적용되어 있음
+	 * 아래 버전을 쓰거나 다른 인코딩 필터를 추가해야하는 경우에만 사용
+	 * 
+	 * @Bean public Filter<?> characterEncodingFilter() { CharacterEncodingFilter
+	 * characterEncodingFilter = new CharacterEncodingFilter();
+	 * 
+	 * characterEncodingFilter.setEncoding("UTF-8");
+	 * characterEncodingFilter.setForceEncoding(true);
+	 * 
+	 * return characterEncodingFilter(); }
+	 * 
+	 * @Bean public HttpMessageConverter<String> responseBodyConverter() { return
+	 * new StringHttpMessageConverter(Charset.forName("UTF-8")); }
+	 */
 }
